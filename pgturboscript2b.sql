175f4c63c540946ace8061f7579c7442b0520fdc
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-19 16:57:12 -0800

    Update pgturboscript2b.sql

--- pgturboscript2b.sql
+++ pgturboscript2b.sql
@@ -1,4 +1,4 @@
 BEGIN;
-SELECT npi_taxonomy.npi, npi_taxonomy.license_num, addresses.id 
-FROM addresses LEFT JOIN npi_taxonomy ON npi_taxonomy.license_num IS NOT NULL;
+SELECT npi_taxonomy.npi, npi_taxonomy.license_num, temp_table.id 
+FROM (SELECT * FROM addresses WHERE addresses.state = 'Kentucky') AS temp_table LEFT JOIN npi_taxonomy ON npi_taxonomy.license_num IS NOT NULL;
 END;
