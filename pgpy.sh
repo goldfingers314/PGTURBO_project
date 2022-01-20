20522b206b97acb978df13deef20bbd4e460d5bb
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-19 19:23:26 -0800

    Update pgpy.sh

--- pgpy.sh
+++ pgpy.sh
@@ -1,12 +1,6 @@
 #!bin/bash
-t="0"
-pgbench -i providerlookup
-sudo touch sample.txt
-while [ $t -le 100000 ];do
-python3 filewriter.py
 sudo service postgresql restart
 sudo chmod -R 777 sample.txt
-pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql providerlookup >> sample.txt
-t=$[$t + 1]
-done
+pgbench -c 10 -j 2 -t 50 -f pgturboscript1.sql providerlookup >> sample.txt
+
 
