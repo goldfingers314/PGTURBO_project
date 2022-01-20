68ae7f7e413e4928c754dd46800475ba1689813a
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-19 16:56:14 -0800

    Update pgturboscript2.sql

--- pgturboscript2.sql
+++ pgturboscript2.sql
@@ -1,8 +1,7 @@
 BEGIN;
-SELECT npi.entity_type,addresses.line1,addresses.line2 
-FROM npi INNER JOIN addresses ON CAST(npi.npi AS VARCHAR) LIKE '2%';
+SELECT shortened_npi.entity_type,addresses.line_1,addresses.line_2 
+FROM (SELECT * FROM npi WHERE npi.addr_mail_phone LIKE '510%') AS shortened_npi INNER JOIN addresses ON CAST(shortened_npi.npi AS VARCHAR) LIKE '2%';
 END;
-
 
 
 
