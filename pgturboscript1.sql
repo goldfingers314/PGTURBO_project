77ffd91b68932ae9fef49937a2dcbaf35da67756
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-22 08:35:08 -0800

    Update pgturboscript1.sql

--- pgturboscript1.sql
+++ pgturboscript1.sql
@@ -39,7 +39,11 @@ BEGIN;
 SELECT npi_taxonomy.npi, npi_taxonomy.license_num, temp_table.id 
 FROM (SELECT * FROM addresses WHERE addresses.state = 'Kentucky') AS temp_table LEFT JOIN npi_taxonomy ON npi_taxonomy.license_num IS NOT NULL;
 END;
+
 BEGIN;
+CREATE INDEX test_id2 ON taxonomy USING BRIN("specialization");
+DROP INDEX test_id2;
+END;
 
 
 
