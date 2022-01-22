f0d67a1a8220121d79fbbd64191b6d3d608f0e77
Srinivasan Sathiamurthy <92271152+goldfingers314@users.noreply.github.com>
2022-01-22 12:19:10 -0500

    Update pgpy.sh

--- pgpy.sh
+++ pgpy.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-brew services restart postgresql
 sudo chmod -R 777 sample.txt
 pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql provider_lookup >> sample.txt
+brew services restart postgresql
