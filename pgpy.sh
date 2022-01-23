d7a8a008a2d7a51d894ce4247bd088f973d87f15
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-22 16:58:32 -0800

    Update pgpy.sh

--- pgpy.sh
+++ pgpy.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 sudo chmod -R 777 sample.csv
-pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql provider_lookup | grep 'including' | grep -Eo '[0-9]*\.[0-9]*'  >> sample.csv  
+pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'  >> sample.csv  
 brew services restart postgresql
