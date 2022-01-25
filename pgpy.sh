937af457d5b6d7e7c1d8814b0b28ac042cca88fa
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-25 08:01:26 -0800

    Update pgpy.sh

--- pgpy.sh
+++ pgpy.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 sudo chmod -R 777 sample.csv
 pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'  >> sample.csv  
-brew services restart postgresql
+pg_isready
+a=$?
+while [ $a -ne 0 ]
+do
+brew services restart postgresql &
+pg_isready 
+a=$?
+done
