4fabfb48dbcc5113291eb34fff0cb7b36727266d
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-25 23:27:05 -0800

    Update pgpy.sh

--- pgpy.sh
+++ pgpy.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 sudo chmod -R 777 sample.csv
-pgbench -c 10 -j 2 -t 50 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'  >> sample.csv  
+touch temp.csv
+pgbench -c 1 t -1 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'   >> sample.csv 
+pgbench -c 1 t -1 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'   >> sample.csv  
+pgbench -c 1 t -1 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'   >> sample.csv 
+pgbench -c 1 t -1 -f pgturboscript2a.sql provider_lookup | grep 'tps' | grep -Eo '[0-9]*\.[0-9]*'   >> sample.csv 
 pg_isready
 a=$?
 while [ $a -ne 0 ]
