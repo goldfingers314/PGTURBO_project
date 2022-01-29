120cf8ba4b44232adb4158f2a2e7999d72ba0698
SudhanvaKulkarni123 <97717328+SudhanvaKulkarni123@users.noreply.github.com>
2022-01-29 08:38:57 -0800

    Update pgturboscript3.sql

--- pgturboscript3.sql
+++ pgturboscript3.sql
@@ -1,5 +1,5 @@
 BEGIN;
 CREATE INDEX test_id2 ON taxonomy USING BRIN("specialization");
-SELECT taxonomy.id,taxonomy.code,COUNT(taxonomy.specialization) FROM taxnomy WHERE taxonomy.specialization LIKE 's%';
+SELECT taxonomy.id,taxonomy.code,COUNT(taxonomy.specialization) FROM taxonomy WHERE taxonomy.specialization LIKE 's%';
 DROP INDEX test_id2;
 END;
